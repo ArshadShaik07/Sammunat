d7ae12ecf6c977bae70a47a7e794d967c361a792
ArshadShaik07 <arshadshaik2007@gmail.com>
2025-12-15 19:18:17 +0530

    added dummy loading state

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,22 @@ import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { useState, useEffect } from "react";
 
 function App() {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 500);
+	}, []);
+
+	if (loading) {
+		return (
+			<div className="min-w-screen min-h-screen overflow-hidden bg-blue-100 flex justify-center items-center">
+				<p className="text-2xl font-semibold">Loading...</p>
+			</div>
+		);
+	}
 	return (
 		<div className="min-w-screen min-h-screen flex flex-col items-center relative">
 			<Navbar />
